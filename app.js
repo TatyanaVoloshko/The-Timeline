6e0e4afeb962f0658259d695854cf74ef0f0f2bf
TatyanaVoloshko <106927709+TatyanaVoloshko@users.noreply.github.com>
2024-02-28 21:59:34 +0100

    created project with EJS

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/public`))
+
+
+app.get("/", (req, res) => {
+    const data = {
+      message: "Timeline",
+      items: [
+        {
+          name: "Michael Choi",
+          createdAt: "23-01-2013",
+          message:
+            "This is my message    This is my message This is my message This is my messageThis is my message This is my message    This is my message This is my message This is my messageThis is my message",
+        },
+        {
+          name: "Maxima Korr",
+          createdAt: "25-02-2013",
+          message:
+            "This is my message    This is my message This is my message This is my messageThis is my message This is my message    This is my message This is my message This is my messageThis is my message",
+        },
+        {
+          name: "Andrew Born",
+          createdAt: "12-08-2013",
+          message:
+            "This is my message    This is my message This is my message This is my messageThis is my message This is my message    This is my message This is my message This is my messageThis is my message",
+        },
+        {
+          name: "Albert Gilmor",
+          createdAt: "15-04-2013",
+          message:
+            "This is my message    This is my message This is my message This is my messageThis is my message This is my message    This is my message This is my message This is my messageThis is my message",
+        },
+      ],
+    };
+     res.render("index", data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+
+
