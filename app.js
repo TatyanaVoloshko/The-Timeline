d62302196def22ca34ea4621a365a9e0cc6ad2c3
TatyanaVoloshko <106927709+TatyanaVoloshko@users.noreply.github.com>
2024-03-01 19:45:01 +0100

    added mongo db and submit posts

--- app.js
+++ app.js
@@ -1,49 +1,81 @@
 const express = require('express')
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const {DB_URI} = process.env;
+
 const app = express()
 const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/public`))
+app.use(express.urlencoded({ extended: true }))
 
 
-app.get("/", (req, res) => {
-    const data = {
-      message: "Timeline",
-      items: [
-        {
-          name: "Michael Choi",
-          createdAt: "23-01-2013",
-          message:
-            "This is my message    This is my message This is my message This is my messageThis is my message This is my message    This is my message This is my message This is my messageThis is my message",
-        },
-        {
-          name: "Maxima Korr",
-          createdAt: "25-02-2013",
-          message:
-            "This is my message    This is my message This is my message This is my messageThis is my message This is my message    This is my message This is my message This is my messageThis is my message",
-        },
-        {
-          name: "Andrew Born",
-          createdAt: "12-08-2013",
-          message:
-            "This is my message    This is my message This is my message This is my messageThis is my message This is my message    This is my message This is my message This is my messageThis is my message",
-        },
-        {
-          name: "Albert Gilmor",
-          createdAt: "15-04-2013",
-          message:
-            "This is my message    This is my message This is my message This is my messageThis is my message This is my message    This is my message This is my message This is my messageThis is my message",
-        },
-      ],
-  };
+
+  async function dbConnect() {
+     await mongoose
+        .connect(DB_URI)
+        .then(() => console.log("Database connection successful"))
+        .catch((error) => {
+            console.log(error.message);
+            process.exit(1);
+        });
+}
+
+dbConnect()
+
+const messageSchema = new mongoose.Schema({
+  name: String,
+  createdAt: String,
+  message: String,
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+
+app.get("/", async (req, res) => {
+ 
+ try {
+   const items = await Message.find();
+
   
-   data.items.sort((a, b) => {
+   items.sort((a, b) => {
      const dateA = a.createdAt.split("-").reverse().join("-");
      const dateB = b.createdAt.split("-").reverse().join("-");
      return new Date(dateB) - new Date(dateA);
    });
 
-     res.render("index", data);
+   res.render("index", { message: "Timeline", items });
+ } catch (error) {
+   console.error(error);
+   res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/newMessage", async (req, res) => {
+  try {
+    const { name, createdAt, message } = req.body;
+
+    if (message.length < 25) {
+      return res
+        .status(400)
+        .send("Message must be at least 25 characters long");
+    }
+
+    const newItem = {
+      name,
+      createdAt,
+      message,
+    };
+
+    await Message.create(newItem);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
